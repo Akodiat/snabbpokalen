12a0d72d475a33566af1bfe611de1b0234e9d2a1
Joakim Bohlin <joakim.bohlin@physics.ox.ac.uk>
2022-09-15 12:38:35 +0200

    Sign up for competitions

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import { getAuth, GoogleAuthProvider, EmailAuthProvider, PhoneAuthProvider, signOut, signInWithRedirect } from "https://www.gstatic.com/firebasejs/9.9.3/firebase-auth.js";
-import { getFirestore, collection, where, getDocs, doc, getDoc, setDoc, addDoc, arrayUnion, updateDoc, query, serverTimestamp, onSnapshot } from "https://www.gstatic.com/firebasejs/9.9.3/firebase-firestore.js";
+import { getFirestore, collection, where, getDocs, doc, getDoc, setDoc, addDoc, deleteDoc, arrayUnion, updateDoc, query, serverTimestamp, onSnapshot } from "https://www.gstatic.com/firebasejs/9.9.3/firebase-firestore.js";
 
 
 const db = getFirestore();
@@ -24,6 +24,9 @@ const userBeltType = document.getElementById('userBeltType');
 const userDojo = document.getElementById('userDojo');
 const userSaveBtn = document.getElementById('userSaveButton');
 
+
+let isEditingCompetition = false;
+
 const createCompetitionDate = M.Datepicker.init(
     document.getElementById('createCompetitionDate'), {
         firstDay: 1,
@@ -39,6 +42,11 @@ const competitionCreateButton = document.getElementById('competitionCreateButton
 const competitionCreateText = document.getElementById('competitionCreateText');
 
 const competitionsList = document.getElementById("competitionsList");
+
+const competitionSignUpSelectCompetition = document.getElementById("competitionSignUpSelectCompetition");
+const competitionSignUpButton = document.getElementById("competitionSignUpButton");
+const competitionSignUpHokei = document.getElementById("competitionSignUpHokei");
+const competitionSignUpJissen = document.getElementById("competitionSignUpJissen");
 
 
 const provider = new GoogleAuthProvider();
@@ -90,54 +98,127 @@ userSaveBtn.onclick = () => {
     });
 }
 
+competitionSignUpButton.onclick = () => {
+    const competitionId = competitionSignUpSelectCompetition.value
+    const regId = competitionId + '_' + currentUser.uid;
+    setDoc(doc(db, 'registrations', regId), {
+        uid: currentUser.uid,
+        competition: competitionId,
+        hokei: competitionSignUpHokei.checked,
+        jissen: competitionSignUpJissen.checked,
+        createdAt: serverTimestamp()
+    }, {merge: true}).then(()=>{
+        M.toast({
+            html: 'Din anmälan har sparats',
+            displayLength: 6000
+        });
+    });
+}
+
 competitionCreateButton.onclick = () => {
     let date = createCompetitionDate.date;
     date.setHours(createCompetitionTime.hours, createCompetitionTime.minutes);
-    const competitionId = currentUser.uid+'_'+date.toISOString();
-    setDoc(doc(db, 'competitions', competitionId), {
+    const competitionId = date.toISOString()+'_'+currentUser.uid;
+    const data = {
         uid: currentUser.uid,
         time: date,
         place: createCompetitionPlace.value,
         infoText: competitionCreateText.value,
         createdAt: serverTimestamp()
-    }, {merge: true}).then(()=>{
+    };
+    setDoc(doc(db, 'competitions', competitionId), data, {merge: true}).then(()=>{
         M.toast({
-            html: 'Den nya tävlingen har lagts till',
+            html: 'Tävlingen har sparats',
             displayLength: 6000
         });
     });
 }
+/*
+function prefillCompetitionInfo(data) {
+    const date = new Date(data.time.seconds*1000)
+    createCompetitionDate.setDate(date);
+    createCompetitionDate.el.value = date.toDateString();
+    const time = `${date.getHours()}:${date.getMinutes()}`;
+    createCompetitionTime.options.defaultTime = time;
+    createCompetitionTime.el.value = time;
+    createCompetitionPlace.value = data.place;
+    competitionCreateText.value = data.infoText ? data.infoText : '';
+    M.updateTextFields();
+}
+*/
+
+function dateStrFromCompetition(competitionData) {
+    let d = new Date(competitionData.time.seconds*1000);
+    return d.toLocaleString('sv-SE', {
+        weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit'
+    });
+}
+
 function loadCompetitions() {
     // Redraw competitions list on every edit
     const q = query(collection(db, "competitions"));
     const unsubscribe = onSnapshot(q, (querySnapshot) => {
         competitionsList.innerHTML = '';
-        querySnapshot.forEach((doc) => {
-            const data = doc.data();
-            let c = document.createElement('li');
-            let d = new Date(data.time.seconds*1000);
-            c.classList.add('collection-item', 'avatar');
-            if (data.uid == currentUser.uid) {
-                let a = document.createElement('a');
-                a.classList.add("btn")
-                a.innerHTML = `<i class="material-icons circle green">start</i>`
-                c.appendChild(a);
-                a.onclick = ()=>alert("hej")
-            } else {
-                c.innerHTML = `<i class="material-icons circle blue">event</i>`
-            }
-            c.innerHTML += `<b class="title">${data.place}</b>
-            <p>${d.toLocaleString('sv-SE', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit'})}<p>${data.infoText ? data.infoText : ''}`
+        querySnapshot.forEach(competition => {
+            const data = competition.data();
+            let item = document.createElement('li');
+            let header = document.createElement('div');
+            header.classList.add("collapsible-header");
+            item.appendChild(header);
+            header.innerHTML = `<i class="material-icons">event</i><b>${data.place}</b>&nbsp;
+            ${dateStrFromCompetition(data)}
+            <span class="badge" data-badge-caption="anmälda">0</span></div>`
+            let body = document.createElement('div');
+            body.classList.add("collapsible-body");
+            body.innerHTML = `<p>${data.infoText ? data.infoText : ''}`;
+            item.appendChild(body);
             // If user is the creator of the competition,
-            // add an edit button
             if (data.uid == currentUser.uid) {
-                let a = document.createElement('a');
-                a.classList.add("secondary-content", "modal-trigger");
-                a.href="#modal1"
-                a.innerHTML = `<i class="material-icons">edit</i>`
-                c.appendChild(a);
+                // Add an edit button
+                /*
+                let editButton = document.createElement('a');
+                editButton.classList.add("modal-trigger");
+                editButton.href="#competitionEditModal"
+                editButton.onclick = ()=>{
+                    prefillCompetitionInfo(data);
+                };
+                editButton.innerHTML = ` <i class="material-icons">edit</i>`
+                header.appendChild(editButton);
+                */
+                let removeButton = document.createElement('a');
+                removeButton.onclick = event=>{
+                    event.stopPropagation();
+                    deleteDoc(doc(db, 'competitions', competition.id)).then(()=>{
+                        M.toast({
+                            html: 'Tävlingen har tagits bort',
+                            displayLength: 6000
+                        });
+                    })
+                };
+                removeButton.innerHTML = ` <i class="material-icons">delete</i>`
+                header.appendChild(removeButton);
+                // Add a start button
+                let startButton = document.createElement('a');
+                startButton.innerHTML = `<i class="material-icons">start</i>`
+                header.appendChild(startButton);
             }
-            competitionsList.appendChild(c);
+            // Add a sign-up button
+            let signUpButton = document.createElement('a');
+            signUpButton.classList.add("modal-trigger");
+            signUpButton.href="#competitionSignUpModal";
+            signUpButton.onclick = (event)=>{
+                competitionSignUpSelectCompetition.innerHTML = '';
+                querySnapshot.forEach(c => {
+                    const d = c.data();
+                    const selected = competition.id == c.id ? ' selected': '';
+                    competitionSignUpSelectCompetition.innerHTML += `<option value="${c.id}"${selected}>${d.place} - ${dateStrFromCompetition(d)}</option>`;
+                });
+                M.FormSelect.init(competitionSignUpSelectCompetition);
+            };
+            signUpButton.innerHTML = `<i class="material-icons">person_add</i>`
+            header.appendChild(signUpButton);
+
+            competitionsList.appendChild(item);
         });
     });
 }
@@ -161,13 +242,4 @@ function loadUserInfo() {
             });
           }
     })
-}
-
-
-
-function signUpToCompetion(competitionId, eventType) {
-    const compRef = updateDoc(doc(db, 'competitions', competitionId),
-    {
-        competitionId: arrayUnion(user.uid)
-    });
 }
